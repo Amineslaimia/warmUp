ba9d8ea40bdc4b16ff86a78c2b92d0a82d3ca8fa
amineslaimia <medamineslaimia@gmail.com>
2019-11-08 10:02:58 +0100

    commit

--- warmUp17.js
+++ warmUp17.js
@@ -4,19 +4,19 @@
 // ​
 // You want to type a string word. Write a function to calculate how much time it takes to type it with one finger.
 // ​
- 
+
 // ​
 // Example 1:
 // ​
 // Input: keyboard = "abcdefghijklmnopqrstuvwxyz", word = "cba"
 // Output: 4
 // Explanation: The index moves from 0 to 2 to write 'c' then to 1 to write 'b' then to 0 again to write 'a'.
-// Total time = 2 + 1 + 1 = 4. 
+// Total time = 2 + 1 + 1 = 4.
 // Example 2:
 // ​
 // Input: keyboard = "pqrstuvwxyzabcdefghijklmno", word = "leetcode"
 // Output: 73
- 
+
 // ​
 // Constraints:
 // ​
@@ -28,5 +28,34 @@
 // ​
 // ​
 // var calculateTime = function(keyboard, word) {
-    
+
 // };
+function each(coll, f) {
+    if (Array.isArray(coll)) {
+      for (var i = 0; i < coll.length; i++) {
+        f(coll[i], i);
+      }
+    } else {
+      for (var key in coll) {
+        f(coll[key], key);
+      }
+    }
+  }
+
+function calculatetime (keyboard, word){
+  var array=keyboard.split('');
+  var objkeyboard={};
+
+     each (array,function(element,i){
+    objkeyboard[element]=i;
+  })
+  var index=objkeyboard[word[0]];
+  var result=0;
+    for (var j =0;j<word.length;j++){
+       result=result+( objkeyboard[word[j]]-index)
+        console.log(result,index,objkeyboard[word[j]])
+       index=objkeyboard[word[j]];
+
+    }
+    return result;
+  }
